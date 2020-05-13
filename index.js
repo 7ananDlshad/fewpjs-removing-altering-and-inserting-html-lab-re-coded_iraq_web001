a86933f10298a757d2d2619b0cf7932cdf50556d
Hanan Dlshad <ensw03217@student.su.edu.krd>
2020-05-13 10:37:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 let newHeader=document.createElement('h1');
 document.body.appendChild(newHeader);
 newHeader.id="victory";
-newHeader.innerHTML="hello";
+newHeader.innerHTML="YOUR-NAME is the champion";
